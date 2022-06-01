feafb7ecee30359950975957e118f27f5640a73d
camaragora <33101467+camaragora@users.noreply.github.com>
2022-06-01 15:49:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('JENKINS YA FIN NÉH');
+  res.send('JENKINS AFAIRE BI YAW LA');
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
